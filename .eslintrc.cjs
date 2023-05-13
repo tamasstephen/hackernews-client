20ef067966caf8b0c0ff5a9492ebe0d9262042bf
tamasstephen <kicsitnagyon@gmail.com>
2023-05-13 12:49:13 +0200

    feat: Add react-qurey

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,10 +5,11 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
     "plugin:jsx-a11y/recommended",
+    "plugin:@tanstack/eslint-plugin-query/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  plugins: ["react-refresh", "jsx-a11y"],
+  plugins: ["react-refresh", "jsx-a11y", "@tanstack/query"],
   rules: {
     "react-refresh/only-export-components": "warn",
   },
