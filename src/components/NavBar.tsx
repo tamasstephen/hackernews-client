9964c085f6b3135311ebc105bd5e2cc5adf3e26b
tamasstephen <kicsitnagyon@gmail.com>
2023-05-14 12:44:23 +0200

    feat: Add responsive nav

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,20 @@
 import { Outlet } from "react-router-dom";
+import { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/joy/List";
-import Divider from "@mui/material/Divider";
+import CssBaseline from "@mui/material/CssBaseline";
+import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  Drawer,
+  IconButton,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
 
 const navItems = [
   {
@@ -32,19 +40,30 @@ const navItems = [
 ];
 
 export default function NavBar() {
+  const [mobileOpen, setMobileOpen] = useState<boolean>(false);
+
+  function handleDrawerToggle() {
+    setMobileOpen(!mobileOpen);
+  }
+
   return (
     <div>
+      <CssBaseline />
       <Box sx={{ display: "flex" }}>
         <AppBar component="nav">
           <Toolbar>
-            <Typography
-              variant="h6"
-              component="div"
-              sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-            >
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               Hacker News
             </Typography>
-
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: "none" } }}
+            >
+              <MenuIcon />
+            </IconButton>
             <Box sx={{ display: { xs: "none", sm: "block" } }}>
               {navItems.map((item) => (
                 <Button key={item.id} sx={{ color: "#fff" }}>
@@ -54,8 +73,49 @@ export default function NavBar() {
             </Box>
           </Toolbar>
         </AppBar>
+        <Box component="nav">
+          <Drawer
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+              display: { xs: "block", sm: "none" },
+              "& .MuiDrawer-paper": { boxSizing: "border-box", width: "80%" },
+            }}
+          >
+            <DrawerItems toggleDrawer={handleDrawerToggle} />
+          </Drawer>
+        </Box>
       </Box>
       <Outlet />
     </div>
+  );
+}
+
+function DrawerItems({ toggleDrawer }: { toggleDrawer: () => void }) {
+  return (
+    <Box
+      onClick={toggleDrawer}
+      sx={{
+        textAlign: "center",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+        display: { xs: "flex", sm: "none" },
+      }}
+    >
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item.id} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item.text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
   );
 }
