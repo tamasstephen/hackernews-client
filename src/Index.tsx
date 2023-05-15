3b17cc962aad632cd44bb04589b11c7597c24c44
tamasstephen <kicsitnagyon@gmail.com>
2023-05-15 13:10:52 +0200

    refactor: Center loading bar

--- src/Index.tsx
+++ src/Index.tsx
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import { getStoryIdsQuery, getTopStoriesQuery } from "./data/queries/queries";
 import ArticleList from "./components/ArticleList";
-import { Alert, CircularProgress, Pagination } from "@mui/material";
+import { Alert, CircularProgress, Grid, Pagination } from "@mui/material";
 import { endpoints } from "./types/types";
 
 function Index({ storyIdKey }: { storyIdKey: keyof typeof endpoints }) {
@@ -13,33 +13,44 @@ function Index({ storyIdKey }: { storyIdKey: keyof typeof endpoints }) {
   } = useQuery(getStoryIdsQuery(storyIdKey));
   const [page, setPage] = useState<number>(1);
   const {
-    data: topStories,
-    isLoading: isLoadingTopStories,
-    isError: isErrorTopStories,
+    data: stories,
+    isLoading: isLoadingStories,
+    isError: isErrorStories,
   } = useQuery({
     ...getTopStoriesQuery(page, topStoryIdArray ?? []),
   });
+
+  useEffect(() => {
+    setPage(1);
+  }, [storyIdKey]);
 
   function handlePagination(_event: React.ChangeEvent<unknown>, value: number) {
     setPage(value);
     window.scrollTo(0, 0);
   }
 
-  useEffect(() => {
-    setPage(1);
-  }, [storyIdKey]);
-
-  if (isLoading || isLoadingTopStories) {
-    return <CircularProgress />;
+  if (isLoading || isLoadingStories) {
+    return (
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: "80vh" }}
+      >
+        <CircularProgress />;
+      </Grid>
+    );
   }
 
-  if (isError || isErrorTopStories) {
+  if (isError || isErrorStories) {
     return <Alert severity="warning">Failed to load stories!</Alert>;
   }
 
   return (
     <>
-      <ArticleList stories={topStories ?? []} />
+      <ArticleList stories={stories ?? []} />
       <Pagination
         sx={{ mt: 4 }}
         count={
