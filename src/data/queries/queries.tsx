4e64811f27460744d02b9c39c9dc601d5ec853c5
tamasstephen <kicsitnagyon@gmail.com>
2023-05-15 10:55:25 +0200

    feat: Add dynamic queries

--- src/data/queries/queries.tsx
+++ src/data/queries/queries.tsx
@@ -1,14 +1,22 @@
 import { getTopStoryIds } from "../api/dataHandlers";
 import { getTopStories } from "../api/dataHandlers";
+import { endpoints, storyKeys } from "../../types/types";
 
-export const topStoryIdsQuery = {
-  queryKey: ["topStoryIds"],
-  queryFn: getTopStoryIds,
-};
-
-export function getTopStoriesQuery(page: number, storyIds: Array<number>) {
+export function getStoryIdsQuery(key: keyof typeof endpoints) {
   return {
-    queryKey: [`topStories_${page}`, page, storyIds],
+    queryKey: [key],
+    queryFn: getTopStoryIds,
+  };
+}
+
+export function getTopStoriesQuery(
+  page: number,
+  storyIds: Array<number>,
+  queryKey?: (typeof storyKeys)[keyof typeof storyKeys]
+) {
+  const key = queryKey ?? "topStories";
+  return {
+    queryKey: [`${key} _${page}`, page, storyIds],
     queryFn: getTopStories,
     enabled: !!storyIds,
   };
